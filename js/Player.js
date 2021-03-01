b3a1c53929fe8a549e36acced79f2eae713721af
yobo114 <70000158+yobo114@users.noreply.github.com>
2021-02-28 19:46:52 -0600

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,54 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  readcars_exodus(){
+    var playerRank = database.ref("cars_exodus")
+    playerRank.on("value",(data)=>{
+      this.rank = data.val()
+
+    })
+    
+  }
+
+ static writecars_exodus(rank){
+    database.ref('/').update({
+      cars_exodus: rank
+   }
+
+   )
+
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
